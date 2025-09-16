1a342397a1d843ec3029fd49fca0c5e75cdfba0d
Letícia Duarte <duartelap@gmail.com>
2025-09-16 02:09:38 +0000

    desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,8 @@ int main(){
     int t = 1; //variável para movimentação da torre
     int b = 1; //variável para movimentação do bispo
     int r = 1; //variável para movimentação da rainha
+    int c1 = 1; //variável para movimentação do cavalo
+    int movimentocompleto = 1; //movimento cavalo
 
     //Movimento Torre
     //5 casas para direita
@@ -32,6 +34,20 @@ int main(){
     for (r = 1; r <= 8; r++)
     {
         printf ("Esquerda\n");
+    }
+    printf ("\n");
+    
+    //Movimento Cavalo
+    //Cavalo se move duas casas para baixo e uma casa para a esquerda
+    printf ("Movimento Cavalo:\n");
+    
+    while (movimentocompleto -- )
+    {
+        for (int c1 = 0; c1 < 2; c1++)
+        {
+            printf ("Baixo\n"); //Imprime "baixo" 2x
+        }
+    printf ("Esquerda\n"); //Imprime "esquerda" 1x
     }
 
 return 0;
